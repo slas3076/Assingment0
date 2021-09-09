1f49187f7a173ee3945ee0cbd84b8292ec8bb1b0
Alexander Schiller-Stefansson <alesc@itu.dk>
2021-09-09 16:19:28 +0200

    Tested and implemented IsLeapYear

--- Assign0.Tests/UnitTest1.cs
+++ Assign0.Tests/UnitTest1.cs
@@ -7,18 +7,80 @@ namespace Assign0.Tests
     public class ProgramTests
     {
         [Fact]
-        public void Main_prints_Hello_World()
+        public void Method_printHelloWorld()
         {
             //Arrange
             var writer = new StringWriter();
             Console.SetOut(writer);
 
             //Act
-            Program.Main(new string[0]);
+            Program.Main(new string[1]{"Print"});
 
             //Assert
             var output = writer.GetStringBuilder().ToString().Trim();
             Assert.Equal("Hello World!", output);
         }
+
+        [Fact]
+        public void LeapYear_is_divisible_by_four()
+        {
+            //Arrange
+            var number1 = 24;
+            var number2 = 48;
+
+            //Act
+            var result1 = Program.isLeapYear(number1);
+            var result2 = Program.isLeapYear(number2);
+
+            //Assert
+            Assert.True(result1);
+            Assert.True(result2);
+        }
+
+        [Fact]
+        public void LeapYear_is_not_divisible_by_four(){
+            //Arrange
+            var number1 = 25;
+            var number2 = 49;
+
+            //Act
+            var result1 = Program.isLeapYear(number1);
+            var result2 = Program.isLeapYear(number2);
+
+            //Assert
+            Assert.False(result1);
+            Assert.False(result2);
+        }
+
+        [Fact]
+        public void LeapYear_is_false_when_divisible_by_100(){
+            //Arrange
+            var number1 = 100;
+            var number2 = 200;
+
+            //Act
+            var result1 = Program.isLeapYear(number1);
+            var result2 = Program.isLeapYear(number2);
+
+            //Assert
+            Assert.False(result1);
+            Assert.False(result2);
+        }
+
+        [Fact]
+        public void LeapYear_is_True_when_divisible_by_400(){
+            //Arrange
+            var number1 = 400;
+            var number2 = 1600;
+
+            //Act
+            var result1 = Program.isLeapYear(number1);
+            var result2 = Program.isLeapYear(number2);
+
+            //Assert
+            Assert.True(result1);
+            Assert.True(result2);
+        }
+    
     }
 }
--- Assign0/Program.cs
+++ Assign0/Program.cs
@@ -6,8 +6,30 @@ namespace Assign0
     {
         public static void Main(string[] args)
         {
-            //Prints Hello World
+            if(args[0] == "Print"){
+                printHelloWorld();
+            }
+
+            if(args[0] == "LeapYear"){
+
+            }
+        }
+
+        public static void printHelloWorld(){
             Console.WriteLine("Hello World!");
         }
+
+        public static bool isLeapYear(int year){
+                if(year % 400 == 0) {
+                    return true;
+                }else if(year % 100 == 0){
+                    return false;
+                }else if(year % 4 == 0){
+                    return true;
+                }else{
+                    return false;
+                }
+        }
+    
     }
 }
